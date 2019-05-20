1b8a8b58cc746f9ccc9f29d4a525b7d4de980bba
Oleksandr Shulha <oleksandr.shulha@student.tuke.sk>
2019-05-20 04:53:10 +0200

    add keyboard, lcd, sendor for card

---
+++ KeyAndLcd/KeyAndLcd.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <Key.h>
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+const byte ROWS = 4;
+const byte COLS = 4;
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte rowPins[ROWS] = {8, 7, 6, 5};
+byte colPins[COLS] = {4, 3, 2, A1};
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void setup(){
+      Serial.begin(9600);
+      lcd.init();
+      lcd.backlight();
+}
+void loop(){
+      lcd.setCursor(0,0);
+      lcd.print("Press any key!"); 
+      char customKey = customKeypad.getKey();
+      if (customKey){
+            Serial.println(customKey);
+            lcd.setCursor(0,1);
+            lcd.print(customKey);
+      }
+}
